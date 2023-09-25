d73f2c7990971b108458f55ba7931e0eec30e191
damhiya <damhiya@gmail.com>
2023-09-25 19:16:28 +0900

    add Interpreter

---
+++ src/Interpreter.hs
@@ -0,0 +1,23 @@
+module Interpreter where
+
+import Syntax qualified as S
+import Evaluation qualified as E
+
+interpret :: E.Env -> S.Term -> IO (E.Value)
+interpret = \env t ->
+  case E.eval env t of
+    Right (E.GetLine ks) -> do
+      s <- getLine
+      go (E.Str s) ks
+    Right (E.PutStr s ks) -> do
+      _ <- putStr s
+      go E.Tt ks
+    Right (E.Pure v) -> pure v
+    Right _ -> error "interpreter error : no IO"
+    Left e  -> error ("eval error : " ++ show e)
+  where
+    go :: E.Value -> [(E.Env, S.Var, S.Term)] -> IO E.Value
+    go v [] = pure v
+    go v ((env, x, t):ks) = do
+      v' <- interpret ((x,v):env) t
+      go v' ks
